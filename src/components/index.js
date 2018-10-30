382759f9e0efe2820fd20761aca169fc54dc5370
Craig Cavalier <craigcav@gmail.com>
2018-10-30 09:52:25 -0400

    apply default theme to all exports

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export {default as EzAlert} from './EzAlert';
+export {default as EzButton} from './EzButton';
+export {EzCard, EzCardSection} from './EzCard';
+export {default as EzHeading} from './EzHeading';
+export {default as EzLabelledItem} from './EzLabelledItem';
+export {default as EzModal} from './EzModal';
+export {EzPage, EzPageSection} from './EzPage';
+export {EzPageContent, EzContentGroup} from './EzPageContent';
+export {default as EzSegmentedControl} from './EzSegmentedControl';
+export {default as EzTextStyle} from './EzTextStyle';
+export {default as EzLayout} from './EzLayout';
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import * as themes from './themes';
+import * as components from './components';
+import {withTheme} from './utils';
 
-export {default as EzAlert} from './components/EzAlert';
-export {default as EzButton} from './components/EzButton';
-export {EzCard, EzCardSection} from './components/EzCard';
-export {default as EzHeading} from './components/EzHeading';
-export {default as EzLabelledItem} from './components/EzLabelledItem';
-export {default as EzModal} from './components/EzModal';
-export {EzPage, EzPageSection} from './components/EzPage';
-export {EzPageContent, EzContentGroup} from './components/EzPageContent';
-export {default as EzSegmentedControl} from './components/EzSegmentedControl';
-export {default as EzTextStyle} from './components/EzTextStyle';
-export {default as EzLayout} from './components/EzLayout';
+export const EzAlert = withTheme(components.EzAlert);
+export const EzButton = withTheme(components.EzButton);
+export const EzCard = withTheme(components.EzCard);
+export const EzCardSection = withTheme(components.EzCardSection);
+export const EzHeading = withTheme(components.EzHeading);
+export const EzLabelledItem = withTheme(components.EzLabelledItem);
+export const EzModal = withTheme(components.EzModal);
+export const EzPage = withTheme(components.EzPage);
+export const EzPageSection = withTheme(components.EzPageSection);
+export const EzPageContent = withTheme(components.EzPageContent);
+export const EzContentGroup = withTheme(components.EzContentGroup);
+export const EzSegmentedControl = withTheme(components.EzSegmentedControl);
+export const EzTextStyle = withTheme(components.EzTextStyle);
+export const EzLayout = withTheme(components.EzLayout);
+
 export {themes};
