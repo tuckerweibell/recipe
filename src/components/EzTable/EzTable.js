d82e68093b683ae4b04b47ecd4fb7608ba9f63b1
Craig Cavalier <craigcav@gmail.com>
2018-11-07 08:47:17 -0500

    intial impl

--- src/components/EzTable/EzTable.js
+++ src/components/EzTable/EzTable.js
@@ -1,21 +1,55 @@
-import styled from 'react-emotion';
-import {base} from './EzTable.styles';
+import React from 'react';
+import {EzCard} from '..';
+import {Table, Th, Td, TableCardSection} from './EzTable.styles';
+
+const Thead = ({columns}) => (
+  <thead>
+    <tr>
+      {columns.map(({heading, numeric}, cellIndex) => (
+        <Th key={cellIndex} numeric={numeric}>
+          {heading}
+        </Th>
+      ))}
+    </tr>
+  </thead>
+);
+
+const Tbody = ({columns, items}) => (
+  <tbody>
+    {items.map((item, rowIndex) => (
+      <tr key={rowIndex}>
+        {columns.map(({accessor, numeric}, cellIndex) => (
+          <Td key={cellIndex} numeric={numeric}>
+            {typeof accessor === 'function' ? accessor({item}) : item[accessor]}
+          </Td>
+        ))}
+      </tr>
+    ))}
+  </tbody>
+);
 
 /**
  * Tables display information in a way that’s easy to scan,
  * so that users can look for patterns and insights.
  * They can be embedded in primary content, such as cards.
  */
-const EzTable = styled('div')(base);
+const EzTable = ({title, subtitle, columns, items}) => {
+  const table = (
+    <Table>
+      <Thead columns={columns} />
+      <Tbody columns={columns} items={items} />
+    </Table>
+  );
 
-EzTable.propTypes = {};
+  if (title) {
+    return (
+      <EzCard title={title} subtitle={subtitle}>
+        <TableCardSection>{table}</TableCardSection>
+      </EzCard>
+    );
+  }
 
-/**
- * defaultProps
- */
-EzTable.defaultProps = {};
+  return table;
+};
 
-/**
- * @component
- */
 export default EzTable;
--- src/components/EzTable/EzTable.styles.js
+++ src/components/EzTable/EzTable.styles.js
@@ -1,3 +1,62 @@
-import {css} from 'react-emotion';
+import styled, {css} from 'react-emotion';
+import {EzCardSection} from '../EzCard';
 
-export const base = () => css();
+const fullBleed = css`
+  padding: 0;
+`;
+
+const spacing = ({
+  theme: {
+    spacing: {lg},
+  },
+}) => css`
+  th,
+  td {
+    &:first-child {
+      padding-left: ${lg};
+    }
+    &:last-child {
+      padding-right: ${lg};
+    }
+  }
+`;
+
+const cell = ({numeric}) =>
+  numeric !== undefined &&
+  css`
+    text-align: right;
+  `;
+
+const heading = ({theme}) => css`
+  color: ${theme.colors.grays[600]};
+`;
+
+const borders = ({theme}) => css`
+  th,
+  td {
+    border-bottom: 1px solid ${theme.colors.grays[300]};
+  }
+
+  tr:last-child td {
+    border-bottom: none;
+  }
+`;
+
+export const Th = styled.th(cell, heading);
+export const Td = styled.td(cell);
+
+export const TableCardSection = styled(EzCardSection)(fullBleed, spacing, borders);
+
+// EzCard checks for a card section (by displayName), and will wrap if it can't find one
+TableCardSection.displayName = EzCardSection.displayName;
+
+const base = css`
+  margin: 0;
+
+  th,
+  td {
+    border: none;
+  }
+`;
+
+export const Table = styled.table(base);
