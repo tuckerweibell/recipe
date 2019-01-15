8efdc51f843b4823716cd49ce19e92920fe9e8fd
Craig Cavalier <craigcav@gmail.com>
2019-01-15 14:24:48 -0500

    update spacing in EzPage

--- src/components/EzPage/styles.tsx
+++ src/components/EzPage/styles.tsx
@@ -3,7 +3,13 @@ import EzHeading from '../EzHeading';
 
 export const childStyles = ({theme}) => css`
   > *:not(:last-child) {
-    margin-bottom: ${theme.spacing.xl2};
+    margin-bottom: ${theme.spacing.sm};
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.medium}) {
+    > *:not(:last-child) {
+      margin-bottom: ${theme.spacing.xl};
+    }
   }
 
   > ${EzHeading} {
