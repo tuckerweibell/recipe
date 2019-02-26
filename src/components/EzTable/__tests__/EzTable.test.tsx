91acc34469473edc2453014aada44b70073f3fea
Craig Cavalier <craigcav@gmail.com>
2019-02-26 14:31:05 -0500

    use react-testing-library instead of enzyme

--- src/components/EzTable/__tests__/EzTable.test.tsx
+++ src/components/EzTable/__tests__/EzTable.test.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import {axe} from 'jest-axe';
 import {visualSnapshots} from 'sosia';
 import Component from 'react-component-component';
+import {fireEvent, cleanup} from 'react-testing-library';
 import markdown from '../EzTable.md';
 import EzTable from '../EzTable';
 import {EzPage, EzCard, EzHeading, EzAlert, EzTextStyle} from '../../index';
-import {mount, renderToHtml} from '../../../jest-globals';
+import {fullRender, renderToHtml} from '../../../jest-globals';
 
 const scope = {EzTable, EzPage, EzCard, EzHeading, EzAlert, EzTextStyle, Component};
 
 describe('EzTable', () => {
   visualSnapshots({markdown, scope});
+  afterEach(cleanup);
 
   const columns = [
     {heading: 'Store name', accessor: ({item}) => item.store},
@@ -22,116 +24,102 @@ describe('EzTable', () => {
     {id: '#007', store: '45 Meadowview Lane', total: 22788, average: 367.55},
   ];
 
-  describe('bulk-select functionality', () => {
-    const findBulkSelectInput = table => table.find(`[aria-label="Select all"]`);
-    const findRowSelectInput = table => table.find(`[aria-label="Select row"]`);
+  function render(props) {
+    const {queryByLabelText, queryAllByLabelText} = fullRender(<EzTable {...props} />);
+    return {
+      bulkSelectInput: queryByLabelText('Select all'),
+      rowSelectInputs: queryAllByLabelText('Select row'),
+    };
+  }
 
-    describe('when a bulk-select functionality is requested', () => {
-      let table;
+  describe('bulk-select', () => {
+    describe('when a bulk-select is requested', () => {
+      const onRowClick = jest.fn();
+      const onBulkSelectClick = jest.fn();
+      const rowIsSelected = jest.fn() as any;
+      const props = {columns, items, onBulkSelectClick, onRowClick, rowIsSelected};
 
       beforeEach(() => {
-        table = mount(
-          <EzTable
-            columns={columns}
-            items={items}
-            onBulkSelectClick={() => null}
-            onRowClick={() => null}
-            rowIsSelected={() => false}
-          />
-        );
+        onRowClick.mockClear();
+        onBulkSelectClick.mockClear();
+        rowIsSelected.mockImplementation(() => false);
       });
 
       it('renders a bulk-select input in a column header', () => {
-        expect(findBulkSelectInput(table)).toHaveLength(1);
+        const {bulkSelectInput} = render(props);
+        expect(bulkSelectInput).not.toBeNull();
       });
 
       it('renders a row-select input for each row', () => {
-        expect(findRowSelectInput(table)).toHaveLength(items.length);
+        const {rowSelectInputs} = render(props);
+        expect(rowSelectInputs).toHaveLength(items.length);
       });
 
       describe('the bulk-select input', () => {
         it('calls the provided click handler when the input state changes', () => {
-          const mockHandler = jest.fn();
+          const {bulkSelectInput} = render(props);
 
-          table.setProps({onBulkSelectClick: mockHandler});
-          findBulkSelectInput(table).simulate('change');
+          bulkSelectInput.click();
 
-          expect(mockHandler).toHaveBeenCalledTimes(1);
-          expect(mockHandler).toHaveBeenCalledWith(expect.any(Object));
+          expect(onBulkSelectClick).toHaveBeenCalledTimes(1);
+          expect(onBulkSelectClick).toHaveBeenCalledWith(expect.any(Object));
         });
 
-        it('is checked when every row has been selected', () => {
-          const itemIds = items.map(({id}) => id);
-          const rowIsSelected = item => itemIds.some(id => item.id === id);
+        describe('checked state', () => {
+          let expectedState;
 
-          table.setProps({rowIsSelected});
+          afterEach(() => {
+            const {bulkSelectInput} = render(props);
+            expect(bulkSelectInput.checked).toBe(expectedState);
+          });
 
-          expect(findBulkSelectInput(table).prop('checked')).toBe(true);
-        });
+          it('is checked when every row has been selected', () => {
+            rowIsSelected.mockImplementation(() => true);
+            expectedState = true;
+          });
 
-        it('is not checked when only some rows are selected', () => {
-          const rowIsSelected = item => item.id === items[0].id;
+          it('is not checked when only some rows are selected', () => {
+            rowIsSelected.mockImplementation(item => item.id === items[0].id);
+            expectedState = false;
+          });
 
-          table.setProps({rowIsSelected});
-
-          expect(findBulkSelectInput(table).prop('checked')).toBe(false);
-        });
-
-        it('is not checked when no rows are selected', () => {
-          const rowIsSelected = item => item.id === 'foobarbaz';
-
-          table.setProps({rowIsSelected});
-
-          expect(findBulkSelectInput(table).prop('checked')).toBe(false);
+          it('is not checked when no rows are selected', () => {
+            rowIsSelected.mockImplementation(() => false);
+            expectedState = false;
+          });
         });
       });
 
       describe('the row-select input', () => {
         it('calls the provided click handler when clicked', () => {
-          const mockHandler = jest.fn();
+          const {rowSelectInputs} = render(props);
 
-          table.setProps({onRowClick: mockHandler});
+          fireEvent.click(rowSelectInputs[0]);
 
-          findRowSelectInput(table)
-            .first()
-            .simulate('change');
-
-          expect(mockHandler).toHaveBeenCalledTimes(1);
-          expect(mockHandler).toHaveBeenCalledWith(expect.any(Object), {item: items[0]});
+          expect(onRowClick).toHaveBeenCalledTimes(1);
+          expect(onRowClick).toHaveBeenCalledWith(expect.any(Object), {item: items[0]});
         });
 
         it('determines its checked state based on the provided function', () => {
-          const rowIsSelected = item => item.id === items[0].id;
+          rowIsSelected.mockImplementation(item => item.id === items[0].id);
 
-          table.setProps({rowIsSelected});
+          const {rowSelectInputs} = render(props);
 
-          expect(
-            findRowSelectInput(table)
-              .at(0)
-              .prop('checked')
-          ).toBe(true);
-          expect(
-            findRowSelectInput(table)
-              .at(1)
-              .prop('checked')
-          ).toBe(false);
+          expect(rowSelectInputs[0].checked).toBe(true);
+          expect(rowSelectInputs[1].checked).toBe(false);
         });
       });
     });
 
     describe('when a bulk-select functionality is not requested', () => {
-      let table;
-
-      beforeEach(() => {
-        table = mount(<EzTable columns={columns} items={items} />);
-      });
+      const {bulkSelectInput, rowSelectInputs} = render({columns, items});
 
       it('does not render a bulk-select input in a column header', () => {
-        expect(findBulkSelectInput(table)).toHaveLength(0);
+        expect(bulkSelectInput).toBeNull();
       });
 
       it('does not render a row-select input for each row', () => {
-        expect(findRowSelectInput(table)).toHaveLength(0);
+        expect(rowSelectInputs[0]).toBeUndefined();
       });
     });
 
