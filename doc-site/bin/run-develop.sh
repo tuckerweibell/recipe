52de52f41fc1d7e972688a0595fb332be1e2ec12
Scott Doxey <neogeek0101@gmail.com>
2019-04-10 16:43:43 -0400

    Added script that will link recipe if not already linked.
    Updated develop npm target.

---
+++ doc-site/bin/run-develop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOC_SITE_DIR=$(cd "$(dirname "$0")" && cd ../ && pwd)
+
+(
+
+    cd "${DOC_SITE_DIR}"
+
+    PACKAGE_NAME="@ezcater/recipe"
+    PACKGE_DIR="${DOC_SITE_DIR}/node_modules/${PACKAGE_NAME}"
+
+    if [[ -d "${PACKGE_DIR}" && -h "${PACKGE_DIR}" ]]
+    then
+        echo "${PACKAGE_NAME} is already linked via npm link"
+    else
+        (cd ../ && npm link)
+        npm link "${PACKAGE_NAME}"
+    fi
+
+    npx gatsby develop
+
+)
