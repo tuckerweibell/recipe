3d214e78fd074e6d2e846b1253d57c9c9c107c46
Craig Cavalier <craigcav@gmail.com>
2019-05-14 14:22:15 -0400

    add workaround for docz overflow bug
    The Preview component in the docz playground overflows on top of the controls to show/hide code examples.

--- doc-site/src/components/Playground/Playground.js
+++ doc-site/src/components/Playground/Playground.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Theme, {components} from 'docz-theme-default';
+import styled from 'react-emotion';
 import DocFrame from './Frame';
 
 const {playground: DoczPlayground} = components;
 
+const Container = styled.div`
+  div[class^='Playground__Preview'] {
+    overflow: scroll;
+  }
+`;
+
 const Playground = ({code, scope}) => (
-  <Theme>
-    <DoczPlayground code={code} scope={{...scope}} wrapper={DocFrame} />
-  </Theme>
+  <Container>
+    <Theme>
+      <DoczPlayground code={code} scope={{...scope}} wrapper={DocFrame} />
+    </Theme>
+  </Container>
 );
 
 export default Playground;
