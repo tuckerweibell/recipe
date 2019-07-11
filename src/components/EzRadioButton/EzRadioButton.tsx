717541fd8a4e4a2fb920d6fccb4bdb4799bba2e3
Scott Doxey <neogeek0101@gmail.com>
2019-07-11 17:41:42 -0400

    Extracted RadioButtonIcon svg into separate file.

--- src/components/EzRadioButton/EzRadioButton.tsx
+++ src/components/EzRadioButton/EzRadioButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from './EzRadioButton.styles';
 
+import RadioButtonIcon from './RadioButtonIcon';
+
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   checked?: boolean;
@@ -21,12 +23,7 @@ const EzRadioButton: React.FC<Props> = ({label, checked, disabled, onChange, ...
       aria-label={label}
       {...rest}
     />
-    <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fillRule="evenodd">
-        <path d="M8 .5a7.5 7.5 0 1 0 0 15 7.5 7.5 0 0 0 0-15z" stroke="#CED4D9" fill="#FFF" />
-        <path d="M8 4a4 4 0 1 1 0 8 4 4 0 1 1 0-8z" fill="#3E90D6" />
-      </g>
-    </svg>
+    <RadioButtonIcon checked={checked} />
   </Container>
 );
 
---
+++ src/components/EzRadioButton/RadioButtonIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const RadioButtonIcon = ({className, checked, width = 16, height = 16}: any) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <g fill="none" fillRule="evenodd">
+      <path d="M8 .5a7.5 7.5 0 1 0 0 15 7.5 7.5 0 0 0 0-15z" stroke="#CED4D9" fill="#FFF" />
+      {checked && <path d="M8 4a4 4 0 1 1 0 8 4 4 0 1 1 0-8z" fill="#3E90D6" />}
+    </g>
+  </svg>
+);
+
+export default RadioButtonIcon;
