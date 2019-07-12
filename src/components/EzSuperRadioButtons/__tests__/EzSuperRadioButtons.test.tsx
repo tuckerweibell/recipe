7d5d1a437a971adc1663e1f1eb77e5a1999986a1
Scott Doxey <neogeek0101@gmail.com>
2019-07-12 12:38:21 -0400

    Added accessibility test.

--- src/components/EzSuperRadioButtons/__tests__/EzSuperRadioButtons.test.tsx
+++ src/components/EzSuperRadioButtons/__tests__/EzSuperRadioButtons.test.tsx
@@ -1,9 +1,43 @@
+import React from 'react';
+import {axe} from 'jest-axe';
 import {visualSnapshots} from 'sosia';
 import markdown from './EzSuperRadioButtons.test.md';
 import EzSuperRadioButtons from '../EzSuperRadioButtons';
+import {renderToHtml} from '../../../jest-globals';
 
 const scope = {EzSuperRadioButtons};
 
 describe('EzSuperRadioButtons', () => {
   visualSnapshots({markdown, scope});
+
+  it('should meet accessibility guidelines', async () => {
+    const onChangeSpy: (value: string) => void = jest.fn();
+
+    const wrapper = renderToHtml(
+      <EzSuperRadioButtons
+        label="Delivery issues"
+        options={[
+          {
+            label: 'first',
+            value: 'firstValue',
+            imageSrc: 'https://image.flaticon.com/icons/svg/865/865149.svg',
+          },
+          {
+            label: 'second',
+            value: 'secondValue',
+            imageSrc: 'https://image.flaticon.com/icons/svg/865/865149.svg',
+          },
+          {
+            label: 'third',
+            value: 'thirdValue',
+            imageSrc: 'https://image.flaticon.com/icons/svg/865/865149.svg',
+          },
+        ]}
+        value="cant-deliver"
+        onChange={onChangeSpy}
+      />
+    );
+    const actual = await axe(wrapper);
+    expect(actual).toHaveNoViolations();
+  });
 });
