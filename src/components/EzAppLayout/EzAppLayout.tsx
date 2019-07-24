bb55b11887fbdfec796df813e3dd45ac95eb3c11
Craig Cavalier <craigcav@gmail.com>
2019-07-24 14:12:45 -0400

    apply Global Styles inside EzAppLayout

--- src/components/EzAppLayout/EzAppLayout.tsx
+++ src/components/EzAppLayout/EzAppLayout.tsx
@@ -1,11 +1,13 @@
 import React, {createContext, useContext} from 'react';
 import {Frame, WidthWrapper} from './EzAppLayout.styles';
+import EzGlobalStyles from '../EzGlobalStyles';
 
 export const EzAppLayoutContext = createContext('full');
 
 const EzAppLayout = ({children, layout}) => {
   return (
     <Frame>
+      <EzGlobalStyles />
       <EzAppLayoutContext.Provider value={layout}>{children}</EzAppLayoutContext.Provider>
     </Frame>
   );
