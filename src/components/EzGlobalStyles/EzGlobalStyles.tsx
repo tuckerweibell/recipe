9593d5253e2965640a5a68be34f6ffd930bfec55
Craig Cavalier <craigcav@gmail.com>
2019-07-26 15:58:32 -0400

    use theme to control base styles
    for easier migration path if/when we change the base font size

--- src/components/EzGlobalStyles/EzGlobalStyles.tsx
+++ src/components/EzGlobalStyles/EzGlobalStyles.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import {standard as theme} from '../../themes';
 import {Global} from '../../styles';
 
 export {default as EzBanner} from '../EzBanner';
 
-// cheat for syntax highlighting
-const css = (...args): string => args[0];
+// cheat for syntax highlighting and minimized css by leaning on babel-plugin-emotion
+function css(...args) {
+  return args.join('').replace(/label:\s*([^\s;\n{]+)\s*;/g, '');
+}
 
-export default () => (
+const EzGlobalStyles = () => (
   <Global
     styles={css`
       html {
         box-sizing: border-box;
-        font-size: 14px;
+        font-size: ${theme.baseFontSize};
         font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
       }
       body {
         margin: 0;
-        color: #565a5c;
+        color: ${theme.colors.text.base};
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
       }
@@ -28,3 +31,5 @@ export default () => (
     `}
   />
 );
+
+export default EzGlobalStyles;
