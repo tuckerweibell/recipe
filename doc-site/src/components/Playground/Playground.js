f470106bff958cf068080079e1932c3f4f795481
Craig Cavalier <craigcav@gmail.com>
2019-12-17 11:57:18 -0500

    fix doc-site playground/editor
    See: https://github.com/ezcater/recipe/issues/263

--- doc-site/src/components/Playground/Playground.js
+++ doc-site/src/components/Playground/Playground.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useRef, useLayoutEffect} from 'react';
+import createEmotion from 'create-emotion';
+import {injectGlobal as injectGlobalForServerRender} from 'emotion';
 import {components} from 'docz-theme-default';
-import styled, {injectGlobal} from 'react-emotion';
+import styled from 'react-emotion';
 import DocFrame from './Frame';
 
 const {playground: DoczPlayground} = components;
@@ -39,13 +41,50 @@ const Container = styled.div`
   }
 `;
 
-injectGlobal`
-  @import url('https://unpkg.com/codemirror@5.42.0/lib/codemirror.css'); @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600'); @import url('https://fonts.googleapis.com/css?family=Inconsolata');
-  .with-overlay{overflow:hidden;} html,body,#root{height:100%;min-height:100%;}
-`;
+// Inject global isn't playing nicely with being run as a loadable component
+// This "Global" component will go away when we move over to emotion 10.
+function Global({styles}) {
+  const ref = useRef(null);
+
+  if (typeof window === 'undefined') injectGlobalForServerRender(styles);
+
+  // use layout effect here to ensure styles will be flushed synchronously, before the browser has a chance to paint.
+  useLayoutEffect(() => {
+    const el = ref.current;
+    const head = el.ownerDocument.head;
+
+    const scopedEmotion = createEmotion(
+      {},
+      {
+        container: head,
+        key: 'recipe-global',
+      }
+    );
+
+    const {injectGlobal} = scopedEmotion;
+
+    injectGlobal(styles);
+
+    return () => {
+      const nodes = head.querySelectorAll('[data-emotion="recipe-global"]');
+      nodes.forEach(node => head.removeChild(node));
+    };
+  }, [styles]);
+
+  return <div ref={ref} />;
+}
 
 const Playground = ({code, scope}) => (
   <Container>
+    <Global
+      styles={`
+      @import url('https://unpkg.com/codemirror@5.42.0/lib/codemirror.css');
+      @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600');
+      @import url('https://fonts.googleapis.com/css?family=Inconsolata');
+      .with-overlay{overflow:hidden;}
+      html,body,#root{height:100%;min-height:100%;}
+    `}
+    />
     <DoczPlayground code={code} scope={{...scope}} wrapper={DocFrame} />
   </Container>
 );
