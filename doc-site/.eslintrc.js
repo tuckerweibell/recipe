0305630158a9bd80745f4bb72775124da6dd911d
Craig Cavalier <craigcav@gmail.com>
2020-01-08 14:00:18 -0500

    wire up static analysis lint plugin to doc-site

--- doc-site/.eslintrc.js
+++ doc-site/.eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', '@ezcater/eslint-plugin-recipe'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
