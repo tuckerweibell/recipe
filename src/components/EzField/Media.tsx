128d9f0f04e37192411b5828f21dcd9401a001ab
Craig Cavalier <craigcav@gmail.com>
2020-05-14 23:32:05 -0400

    fix: leaking styles in visual regression media component
    when running in JSDOM the iframe ownerDocument is not a unique document object and gets shared across tests (causing styles to leak).
    
    Calling `current.sheet.flush()` when the iframe content unmounts to prevent styles leaking

--- src/components/EzField/Media.tsx
+++ src/components/EzField/Media.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState, useEffect} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {createPortal} from 'react-dom';
 import {CacheProvider} from '@emotion/core';
 // eslint-disable-next-line
@@ -11,16 +11,23 @@ const sizes = {
   large: {width: 1920, height: 1080}, // laptop e.g. dell xps 13
 };
 
+const IFrameContent = ({iframeEl, children}) => {
+  const cache = useRef(createCache({container: iframeEl.ownerDocument.head, key: 'frame'}));
+
+  // Remove any injected stylesheets from the page when the component is unmounted
+  React.useEffect(() => () => cache.current.sheet.flush());
+
+  return (
+    <>{createPortal(<CacheProvider value={cache.current}>{children}</CacheProvider>, iframeEl)}</>
+  );
+};
+
 const Media = ({size, children}) => {
   const iframeEl = useRef(null);
   const linkRef = useRef(null);
-  const [container, setContainer] = useState(null);
   const [src, setSrc] = useState(undefined);
 
-  const createStylesCache = useCallback(
-    () => createCache({container: container.ownerDocument.head, key: 'frame'}),
-    [container]
-  );
+  const [container, setContainer] = useState(null);
 
   useEffect(() => {
     setContainer(iframeEl.current);
@@ -36,26 +43,23 @@ const Media = ({size, children}) => {
       scrolling="no"
       src={src}
     >
-      {iframeEl.current &&
-        !src &&
-        createPortal(
-          <CacheProvider value={createStylesCache()}>
-            <link
-              href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i&display=swap"
-              rel="stylesheet"
-              ref={linkRef}
-            />
-            <EzGlobalStyles />
-            {children}
-            {linkRef.current &&
-              setSrc(
-                `data:text/html,${encodeURIComponent(
-                  `<html>${linkRef.current.ownerDocument.head.outerHTML}<body>${iframeEl.current.innerHTML}</body></html>`
-                )}`
-              )}
-          </CacheProvider>,
-          iframeEl.current
-        )}
+      {iframeEl.current && !src && (
+        <IFrameContent iframeEl={iframeEl.current}>
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i&display=swap"
+            rel="stylesheet"
+            ref={linkRef}
+          />
+          <EzGlobalStyles />
+          {children}
+          {linkRef.current &&
+            setSrc(
+              `data:text/html,${encodeURIComponent(
+                `<html>${linkRef.current.ownerDocument.head.outerHTML}<body>${iframeEl.current.innerHTML}</body></html>`
+              )}`
+            )}
+        </IFrameContent>
+      )}
     </iframe>
   );
 };
