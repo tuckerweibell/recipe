b588c36eb993c2c72d6fe87c36b8c9009e3c2bb6
Craig Cavalier <craigcav@gmail.com>
2020-06-09 09:53:11 -0400

    fix: EzPageHeader selected Link color
    selected link text should be black (i.e. using links or buttons should render the style)
    
    closes #348

--- src/components/EzPageHeader/Tabs.styles.ts
+++ src/components/EzPageHeader/Tabs.styles.ts
@@ -18,7 +18,8 @@ const activeTab = ({theme, active}) =>
   css`
     box-shadow: inset 0 -2px 0 0 ${theme.colors.interactive.focus.outline};
 
-    && > * {
+    && > *,
+    && a {
       color: ${theme.colors.text.base};
 
       :focus {
