3a2fa665309e2b2fb26c092d76fb7c211e7c087c
Craig Cavalier <craigcav@gmail.com>
2020-06-29 13:56:36 -0400

    improve layout of EzFlashMessage
    - extends EzCard for more consistent usage of spacing.
    - uses EzLayout for more consistent spacing and positioning
    - fixes position of dismiss button. Closes #320

--- src/components/EzFlashMessage/EzFlashMessage.styles.tsx
+++ src/components/EzFlashMessage/EzFlashMessage.styles.tsx
@@ -1,20 +1,22 @@
 import {css} from '@emotion/core';
 import variant from 'styled-component-variant';
+import {EzCard} from '../EzCard';
 import styled from '../../themes/styled';
 
 const coloredStyles = getColors => ({theme: {colors}}) => {
   const {background, foreground, border} = getColors(colors);
   return css`
     background-color: ${background};
-    border-left: 5px solid ${foreground};
-    box-shadow: inset -1px 0 0 ${border}, inset 0 1px 0 ${border}, inset 0 -1px 0 ${border};
+    border-color: ${border};
+    border-left-width: 5px;
+    border-left-color: ${foreground};
     svg path {
       fill: ${foreground};
     }
   `;
 };
 
-const useStyles = variant('use', {
+const accent = variant('use', {
   error: coloredStyles(colors => colors.destructive),
   info: coloredStyles(colors => colors.info),
   success: coloredStyles(colors => colors.success),
@@ -22,7 +24,7 @@ const useStyles = variant('use', {
 });
 
 export type FlashMessageContainerProps = {
-  use: string;
+  use: 'success' | 'error' | 'warning' | 'info';
 };
 
 export type FlashMessageProps = FlashMessageContainerProps & {
@@ -31,31 +33,6 @@ export type FlashMessageProps = FlashMessageContainerProps & {
   onDismiss?: () => void;
 };
 
-export const FlashMessageContainer = styled.div<FlashMessageContainerProps>`
-  border-radius: ${props => props.theme.borderRadius[1]};
-  border-width: 1px;
-  display: flex;
-  padding: ${props => props.theme.spacing.sm};
-  padding-right: ${props => props.theme.spacing.lg};
-  ${useStyles};
-  p:last-of-type {
-    margin-bottom: 0;
-  }
-`;
-
-export const FlashMessageContents = styled.div`
-  margin: 0 ${props => props.theme.spacing.sm};
-  flex: 1;
-`;
-
-export const FlashMessageHeadline = styled.p`
-  margin-bottom: ${props => props.theme.spacing.xs};
-  font-weight: bold;
-`;
-
-export const CloseButton = styled.button`
-  border: 0;
-  background: none;
-  outline: none;
-  cursor: pointer;
+export const FlashMessageContainer = styled(EzCard)<FlashMessageContainerProps>`
+  ${accent};
 `;
--- src/components/EzFlashMessage/EzFlashMessage.tsx
+++ src/components/EzFlashMessage/EzFlashMessage.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import {
-  FlashMessageContainer,
-  FlashMessageContents,
-  FlashMessageHeadline,
-  FlashMessageProps,
-} from './EzFlashMessage.styles';
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
+import {FlashMessageContainer, FlashMessageProps} from './EzFlashMessage.styles';
+import EzLayout from '../EzLayout';
+import EzHeading from '../EzHeading';
+
 import CloseButton from '../CloseButton';
 import {InfoIcon, ErrorIcon, SuccessIcon} from '../Icons';
 
@@ -21,12 +20,16 @@ const icons = {
 const EzFlashMessage: React.FC<FlashMessageProps> = props => {
   return (
     <FlashMessageContainer use={props.use}>
-      {icons[props.use]}
-      <FlashMessageContents>
-        {props.headline && <FlashMessageHeadline>{props.headline}</FlashMessageHeadline>}
-        {props.children}
-      </FlashMessageContents>
-      {props.onDismiss && <CloseButton label="Close" color="red" onClick={props.onDismiss} />}
+      <EzLayout layout="basic" alignY="top">
+        {icons[props.use]}
+        <EzLayout layout="stack" css={{flex: 1}}>
+          {props.headline && <EzHeading size="3">{props.headline}</EzHeading>}
+          <div>{props.children}</div>
+        </EzLayout>
+        {props.onDismiss && (
+          <CloseButton css={{alignSelf: 'start'}} label="Close" onClick={props.onDismiss} />
+        )}
+      </EzLayout>
     </FlashMessageContainer>
   );
 };
