48f146bd7cdb018b2671b2192060af2bbf7d7f21
Craig Cavalier <craigcav@gmail.com>
2020-07-22 17:27:01 -0400

    extract useMenuTrigger from useCombobox
    datepicker and select aren't comboboxes - datepicker is a dialog (containing a calendar) and select is a listbox implementation.
    
    Since we're going to be building out a combobox for autosuggest, we'll want to extract the common logic into base hooks.

--- src/components/EzField/EzCombobox.ts
+++ src/components/EzField/EzCombobox.ts
@@ -1,6 +1,11 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import {useDisclosureState, useDisclosureContent} from 'reakit/Disclosure';
-import {useAllCallbacks, useOnClickOutside, useEventListenerOutside} from '../../utils/hooks';
+import {
+  useAllCallbacks,
+  useOnClickOutside,
+  useEventListenerOutside,
+  useUniqueId,
+} from '../../utils/hooks';
 
 export const useComboboxState = () => {
   // eslint-disable-next-line @typescript-eslint/camelcase
@@ -58,5 +63,88 @@ export const useComboboxFlyout = (options, htmlProps = {}) => {
   useDisclosureContent(options, htmlProps);
   return {
     id: options.unstable_hiddenId,
+  };
+};
+
+export const useMenuTrigger = (state: OverlayTriggerState) => {
+  const menuTriggerId = useUniqueId();
+  const {triggerProps, overlayProps} = useOverlayTrigger(state);
+
+  const keyMap = {
+    Escape: state.close,
+    ArrowUp: state.open,
+    ArrowDown: state.open,
+    ' ': state.open,
+  };
+
+  return {
+    menuTriggerProps: {
+      ...triggerProps,
+      id: menuTriggerId,
+      onMouseDown() {
+        state.toggle();
+      },
+      onKeyDown(e) {
+        const action = keyMap[e.key];
+        if (e.defaultPrevented || !action) return;
+        if (!state.isOpen) e.preventDefault();
+        action(e);
+      },
+    },
+    menuProps: {
+      ...overlayProps,
+      'aria-labelledby': menuTriggerId,
+    },
+  };
+};
+
+const useOverlayTrigger = (state: OverlayTriggerState) => {
+  const overlayId = useUniqueId();
+  const {isOpen} = state;
+
+  return {
+    triggerProps: {
+      'aria-expanded': isOpen,
+      'aria-owns': isOpen ? overlayId : null,
+    },
+    overlayProps: {
+      id: overlayId,
+    },
+  };
+};
+
+interface OverlayTriggerState {
+  /** Whether the overlay is currently open. */
+  readonly isOpen: boolean;
+  /** Opens the overlay. */
+  open(): void;
+  /** Closes the overlay. */
+  close(): void;
+  /** Toggles the overlay's visibility. */
+  toggle(): void;
+}
+
+/**
+ * Manages state for a menu trigger. Tracks whether the menu is currently open,
+ * and controls which item will receive focus when it opens.
+ */
+export const useMenuTriggerState = (): OverlayTriggerState => {
+  return useOverlayTriggerState();
+};
+
+const useOverlayTriggerState = (): OverlayTriggerState => {
+  const [isOpen, setOpen] = useState(false);
+
+  return {
+    isOpen,
+    open() {
+      setOpen(true);
+    },
+    close() {
+      setOpen(false);
+    },
+    toggle() {
+      setOpen(!isOpen);
+    },
   };
 };
