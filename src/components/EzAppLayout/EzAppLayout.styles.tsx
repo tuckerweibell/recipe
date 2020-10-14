1368b48c7b07aada097942638c0b21d41a145e41
Craig Cavalier <craigcav@gmail.com>
2020-10-14 14:58:54 -0400

    use tokens in AppLayout

--- src/components/EzAppLayout/EzAppLayout.styles.tsx
+++ src/components/EzAppLayout/EzAppLayout.styles.tsx
@@ -1,11 +1,12 @@
 import {css} from '@emotion/core';
-import styled from '../../themes/styled';
+import styled from '@emotion/styled';
+import './vars.css';
 
-const centeredStyles = ({theme, width}: {theme: any; width?: string}) =>
+const centeredStyles = ({width}: {width?: string}) =>
   width === 'centered' &&
   css`
     margin: 0 auto;
-    max-width: ${theme.pageContentWidth.base};
+    max-width: var(--recipe-app-layout-centered-max-width);
   `;
 
 export const Frame = styled.div`
