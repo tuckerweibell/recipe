33386bacfd57456b60d66bec76da6a9bbb309702
Craig Cavalier <craigcav@gmail.com>
2020-10-19 14:23:30 -0400

    use tokens in EzFlashMessage

--- src/components/EzFlashMessage/EzFlashMessage.styles.tsx
+++ src/components/EzFlashMessage/EzFlashMessage.styles.tsx
@@ -1,26 +1,42 @@
 import {css} from '@emotion/core';
+import styled from '@emotion/styled';
 import variant from 'styled-component-variant';
 import {EzCard} from '../EzCard';
-import styled from '../../themes/styled';
+import './vars.css';
 
-const coloredStyles = getColors => ({theme: {colors}}) => {
-  const {background, foreground, border} = getColors(colors);
-  return css`
-    background-color: ${background};
-    border-color: ${border};
-    border-left-width: 5px;
-    border-left-color: ${foreground};
-    svg path {
-      fill: ${foreground};
-    }
-  `;
-};
+const success = () => css`
+  --recipe-flash-message-background-color: var(--recipe-semantic-positive-color-background);
+  --recipe-flash-message-border-color: var(--recipe-semantic-positive-color-border);
+  --recipe-flash-message-text-color: var(--recipe-semantic-positive-color-text-small);
+  --recipe-flash-message-accent-color: var(--recipe-semantic-positive-color-default);
+`;
+
+const error = () => css`
+  --recipe-flash-message-background-color: var(--recipe-semantic-negative-color-background);
+  --recipe-flash-message-border-color: var(--recipe-semantic-negative-color-border);
+  --recipe-flash-message-text-color: var(--recipe-semantic-negative-color-text-small);
+  --recipe-flash-message-accent-color: var(--recipe-semantic-negative-color-default);
+`;
+
+const warning = () => css`
+  --recipe-flash-message-background-color: var(--recipe-semantic-caution-color-background);
+  --recipe-flash-message-border-color: var(--recipe-semantic-caution-color-border);
+  --recipe-flash-message-text-color: var(--recipe-semantic-caution-color-text-small);
+  --recipe-flash-message-accent-color: var(--recipe-semantic-caution-color-default);
+`;
+
+const info = () => css`
+  --recipe-flash-message-background-color: var(--recipe-semantic-informative-color-background);
+  --recipe-flash-message-border-color: var(--recipe-semantic-informative-color-border);
+  --recipe-flash-message-text-color: var(--recipe-semantic-informative-color-text-small);
+  --recipe-flash-message-accent-color: var(--recipe-semantic-informative-color-default);
+`;
 
 const accent = variant('use', {
-  error: coloredStyles(colors => colors.destructive),
-  info: coloredStyles(colors => colors.info),
-  success: coloredStyles(colors => colors.success),
-  warning: coloredStyles(colors => colors.warning),
+  error,
+  info,
+  success,
+  warning,
 });
 
 export type FlashMessageContainerProps = {
@@ -35,4 +51,11 @@ export type FlashMessageProps = FlashMessageContainerProps & {
 
 export const FlashMessageContainer = styled(EzCard)<FlashMessageContainerProps>`
   ${accent};
+  background-color: var(--recipe-flash-message-background-color);
+  border-color: var(--recipe-flash-message-border-color);
+  border-left-width: var(--recipe-flash-message-accent-border-size);
+  border-left-color: var(--recipe-flash-message-accent-color);
+  svg path {
+    fill: var(--recipe-flash-message-accent-color);
+  }
 `;
