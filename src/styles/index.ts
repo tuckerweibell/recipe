97d02ce9d7a416d22443b38339750f6352e954ae
Craig Cavalier <craigcav@gmail.com>
2020-10-23 12:09:52 -0400

    remove no longer used psuedo class helper
    these styles are all controlled by the theme now, so we no longer need a helper.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,4 +4,3 @@ import * as keyframes from './keyframes';
 export {responsive, keyframes};
 export {default as darken} from './darken';
 export {default as hideVisually} from './hideVisually';
-export {default as pseudoClasses} from './pseudoClasses';
