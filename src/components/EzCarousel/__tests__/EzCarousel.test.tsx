ce8d0c4678bb5b567867f7347282c1bb8f1a14a9
Craig Cavalier <craigcav@gmail.com>
2020-11-10 16:22:06 -0500

    add full card-based carousel example

--- src/components/EzCarousel/__tests__/EzCarousel.test.tsx
+++ src/components/EzCarousel/__tests__/EzCarousel.test.tsx
@@ -2,9 +2,20 @@ import {visualSnapshots} from 'sosia';
 import markdown from '../EzCarousel.md';
 import EzCarousel from '../EzCarousel';
 import Placeholder from '../../../../doc-site/src/components/Placeholder';
-import {EzLayout, EzHeading} from '../../index';
+import {EzCard, EzLayout, EzHeading, EzPage} from '../../index';
 
-const scope = {EzCarousel, Placeholder, EzLayout, EzHeading};
+const scope = {
+  EzCarousel,
+  Placeholder,
+  EzLayout,
+  EzHeading,
+  EzCard,
+  EzPage,
+  withPrefix() {
+    // swap the provided images for placeholder values
+    return 'https://via.placeholder.com/800+x+400/00b373/FFFFFF?text=800+x+400';
+  },
+};
 
 describe('EzCarousel', () => {
   visualSnapshots({markdown, scope});
