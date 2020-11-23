65b47bf17b005eadb38291b7957889758d17b721
Craig Cavalier <craigcav@gmail.com>
2020-11-23 12:03:57 -0500

    refactor carousel logic for applying snap-points
    This is a pure refactor (no logic changed).
    
    We'll need to apply similar logic for resetting the slide-width `nth-of-type` selectors, so this refactor is a step in that direction.

--- src/components/EzCarousel/EzCarousel.styles.ts
+++ src/components/EzCarousel/EzCarousel.styles.ts
@@ -24,25 +24,36 @@ export const nextStyle = (isFirst): Interpolation =>
 export const prevStyle = (isLast): Interpolation =>
   cx({...nextPrevStyle, left: 0}, isLast && hiddenStyle);
 
-/**
- * Provisions enough space for all the slides, including optional space between slides and the optional next/previous slide preview
- */
-const calcSlideWidthStyles = options => {
-  const {n, gap = false, peek = false, count} = options;
+const getButtonWidthCalc = ({gap}) =>
+  gap
+    ? `(var(--recipe-carousel-button-width) + var(--recipe-carousel-item-double-margin))`
+    : 'var(--recipe-carousel-button-width)';
 
+const getSlideWidthCalc = ({gap, peek, n}) => {
   let slideWidth = `${100 / n}%`;
 
   // subtract the total margin (left & right) between items (i.e `n-1`), spread over each item (i.e. `/ n`)
   if (gap)
     slideWidth = `${slideWidth} - (var(--recipe-carousel-item-double-margin) * ${n - 1} / ${n})`;
 
-  let buttonWidth = 'var(--recipe-carousel-button-width)';
-
-  if (gap) buttonWidth = `(${buttonWidth} + var(--recipe-carousel-item-double-margin))`;
-
   // when peek is enabled, we need to provision space for the next/prev button (i.e. `* 2` for two buttons)
   // so we divide the button width + margin between each item
-  if (peek) slideWidth = `${slideWidth} - (${buttonWidth} * 2 / ${n})`;
+  if (peek) slideWidth = `${slideWidth} - (${getButtonWidthCalc({gap})} * 2 / ${n})`;
+
+  return slideWidth;
+};
+
+const setFlexBasis = ({gap, peek, n}) => ({
+  flexBasis: `calc(${getSlideWidthCalc({gap, peek, n})})`,
+});
+
+/**
+ * Provisions enough space for all the slides alongside the next/previous slide preview
+ */
+const resizeForPeek = options => {
+  const {n, count} = options;
+  const slideWidth = getSlideWidthCalc(options);
+  const buttonWidth = getButtonWidthCalc(options);
 
   // grow the items on the first and last page to account for only having a single button
   const firstAndLast = {flexBasis: `calc(${slideWidth} + (${buttonWidth} / ${n}))`};
@@ -52,76 +63,69 @@ const calcSlideWidthStyles = options => {
   const padLastPageItems = n - lastPageCount > 0 && count / n > 2;
 
   return cx(
-    {flexBasis: `calc(${slideWidth})`},
-    peek &&
-      cx(
-        {
-          [`:nth-of-type(-n+${n})`]: firstAndLast,
-          [`:nth-last-of-type(-n+${lastPageCount})`]: firstAndLast,
-        },
-        // if last page doesn't contain n items, we need to pad the remaining space
-        // to ensure that space provisioned next/prev buttons is still balanced
-        // Note: this only applies if there are more than two pages, since the items on
-        // the first and last page are evenly sized (it's only the "middle" pages of the
-        // carousel that have smaller items).
-        // Unfortunately, scroll containers don't like having right-margin on the last item
-        // see: https://github.com/w3c/csswg-drafts/issues/129#issuecomment-501855090
-        // so instead, we'll pad it out with a pseudo element
-        {
-          ':last-of-type:before': {
-            ...pseudo,
-            content: padLastPageItems ? pseudo.content : 'none',
-            left: '100%',
-            // allocate the remaining width
-            width: `calc((${buttonWidth} / ${n}) * ${n - lastPageCount})`,
-            height: '1px',
-          },
-        }
-      )
+    {
+      [`:nth-of-type(-n+${n})`]: firstAndLast,
+      [`:nth-last-of-type(-n+${lastPageCount})`]: firstAndLast,
+    },
+    // if last page doesn't contain n items, we need to pad the remaining space
+    // to ensure that space provisioned next/prev buttons is still balanced
+    // Note: this only applies if there are more than two pages, since the items on
+    // the first and last page are evenly sized (it's only the "middle" pages of the
+    // carousel that have smaller items).
+    // Unfortunately, scroll containers don't like having right-margin on the last item
+    // see: https://github.com/w3c/csswg-drafts/issues/129#issuecomment-501855090
+    // so instead, we'll pad it out with a pseudo element
+    {
+      ':last-of-type:before': {
+        ...pseudo,
+        content: padLastPageItems ? pseudo.content : 'none',
+        left: '100%',
+        // allocate the remaining width
+        width: `calc((${buttonWidth} / ${n}) * ${n - lastPageCount})`,
+        height: '1px',
+      },
+    }
   );
 };
 
 const mid = n => `${n}n - ${Math.floor(n / 2)}`;
 
+const snapPoints = ({n, gap, reset}: {n: number; gap: boolean; reset?: boolean}): Interpolation => {
+  // when centering on the pseudo element, we need to offset for the gap between items (if one is applied)
+  const right = gap ? 'calc(var(--recipe-carousel-item-margin) * -1)' : 0;
+  const usePseudo = n % 2 === 0;
+
+  return {
+    // target the mid-point of each page of carousel items, setting the scrollSnapAlign to center (or resetting to initial)
+    [`:nth-of-type(${mid(n)})${usePseudo ? ':after' : ''}`]: {
+      ...(usePseudo ? {right, position: 'absolute', content: reset ? 'none' : "''"} : {}),
+      scrollSnapAlign: reset ? 'initial' : 'center',
+    },
+    // for lists that aren't evenly divided, ensure the last item snaps to the end of the list
+    ':last-of-type': {scrollSnapAlign: 'start'},
+  };
+};
+
 const slidesPerPageStyles = options => {
-  const {slidesPerPage = 1} = options;
+  const {slidesPerPage = 1, gap, peek} = options;
 
-  // when centering on the pseudo element, we need to offset for the gap (if one is applied)
-  const right = options.gap ? 'calc(var(--recipe-carousel-item-margin) * -1)' : 0;
-
-  const style = n =>
+  const style = (n: number, prev?: number) =>
     cx(
-      {
-        position: 'relative',
-        flexShrink: 0,
-        // ignore the intrinsic width of the carousel item content, so flex-basis is king
-        width: 0,
-
-        ...(n % 2 === 0
-          ? {[`:nth-of-type(${mid(n)}):after`]: {...pseudo, right, scrollSnapAlign: 'center'}}
-          : {[`:nth-of-type(${mid(n)})`]: {scrollSnapAlign: 'center'}}),
-
-        // for lists that aren't evenly divided, ensure the last item snaps to the end of the list
-        ':last-of-type': {scrollSnapAlign: 'start'},
-      },
-      calcSlideWidthStyles({n, ...options})
+      setFlexBasis({n, gap, peek}),
+      // reset the snap-points from the previous breakpoint.
+      prev && snapPoints({n: prev, gap, reset: true}),
+      snapPoints({n, gap}),
+      peek && resizeForPeek({n, ...options})
     );
 
   if (typeof slidesPerPage === 'number') return style(slidesPerPage);
 
-  const breakpoints = Object.values(slidesPerPage).reduce((res: any, value: number, i, arr) => {
-    if (i === 0) return {[value]: style(value)};
-
-    const n = arr[i - i] as number;
-
-    // restore the initial value of snap-points to override the previous breakpoint.
-    const reset =
-      n % 2 === 0
-        ? {[`:nth-of-type(${mid(n)}):after`]: {content: 'none'}}
-        : {[`:nth-of-type(${mid(n)})`]: {scrollSnapAlign: 'initial'}};
-
-    return {...res, [value]: Object.assign(reset, style(value))};
-  }, {});
+  const breakpoints = Object.values(slidesPerPage).reduce(
+    (res: object, n: number, i, arr: number[]) => {
+      return {...res, [n]: style(n, i > 0 ? arr[i - i] : undefined)};
+    },
+    {}
+  );
 
   return responsive('slidesPerPage', breakpoints)({slidesPerPage});
 };
@@ -145,6 +149,8 @@ export const listStyle = (options): Interpolation => ({
 
 export const listItemStyle = (options): Interpolation =>
   cx(
+    // Note: width=0 to ignore the intrinsic width of the carousel item content, so flex-basis takes precedence
+    {position: 'relative', flexShrink: 0, width: 0},
     options.peek && {
       ':first-of-type': {marginLeft: 0},
       ':last-of-type': {marginRight: 0},
