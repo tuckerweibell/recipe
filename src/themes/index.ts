3ff814254f5f777b5657fac869bf769c21d5cc78
Craig Cavalier <craigcav@gmail.com>
2020-12-08 10:12:12 -0500

    mark imports from themes/standard as deprecated

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,13 @@
-import * as standard from './standard';
+import * as legacyTheme from './standard';
 
-export {standard};
+/**
+ * @deprecated The standard theme is no longer used by Recipe and is no longer maintained in this package.
+ *
+ * Import from `@recipe-ui/legacy-theme` directly for backwards-compatibility with Recipe versions prior to v11,
+ * or define theme styles local to your project.
+ *
+ * The standard theme may be removed from the `@ezcater/recipe` package in a future release.
+ *
+ * See: https://github.com/ezcater/recipe/releases/tag/11.0.0 for more information.
+ */
+export const standard = legacyTheme;
