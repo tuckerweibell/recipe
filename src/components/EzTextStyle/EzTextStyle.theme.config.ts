05bf98323ea66a57cf7f03076000f2fcbbfa7345
Craig Cavalier <craigcav@gmail.com>
2021-03-10 09:56:51 -0500

    create `mergeCss` utility

--- src/components/EzTextStyle/EzTextStyle.theme.config.ts
+++ src/components/EzTextStyle/EzTextStyle.theme.config.ts
@@ -1,15 +1,11 @@
-import {createCss} from '@stitches/core';
-import root from '../theme.config';
+import {mergeCss} from '../theme.config';
 
-export default createCss({
-  ...root.config,
+export default mergeCss({
   theme: {
     fontWeights: {
-      ...root.config.theme.fontWeights,
       'text-strong': '$fontWeights$bold',
     },
     colors: {
-      ...root.config.theme.colors,
       'text-subdued': '$deemphasisText',
     },
   },
--- src/components/theme.config.ts
+++ src/components/theme.config.ts
@@ -1,7 +1,15 @@
-/* eslint-disable */
-import {createCss} from '@stitches/core';
+/* eslint-disable filenames/match-exported */
+import {
+  createCss,
+  TConditions,
+  TTheme,
+  TThemeMap,
+  CSSPropertiesToTokenScale,
+  TStyledSheet,
+  IConfig,
+} from '@stitches/core';
 
-export default createCss({
+const stitches = createCss({
   theme: {
     colors: {
       gray600: '#8b99a6',
@@ -20,3 +28,55 @@ export default createCss({
     large: '@media (min-width: 1061px)',
   },
 });
+
+type BaseConfig = typeof stitches.config;
+
+export default stitches;
+interface IObject {
+  [key: string]: any;
+}
+
+type TUnionToIntersection<U> = (U extends any
+? (k: U) => void
+: never) extends (k: infer I) => void
+  ? I
+  : never;
+
+export const deepMerge = <T extends IObject[]>(...objects: T): TUnionToIntersection<T[number]> => {
+  const result: any = {};
+
+  function merge(obj) {
+    Object.entries(obj).forEach(([key, value]) => {
+      result[key] =
+        typeof value === 'object' && typeof result[key] === 'object'
+          ? deepMerge(result[key], value)
+          : value;
+    });
+  }
+
+  objects.forEach(merge);
+
+  return result;
+};
+
+/**
+ * Extends the base stitches configuration with additional theme tokens.
+ */
+export function mergeCss<
+  Conditions extends TConditions,
+  Theme extends TTheme = Record<string, unknown>,
+  Utils = Record<string, unknown>,
+  Prefix = '',
+  ThemeMap extends TThemeMap = CSSPropertiesToTokenScale
+>(
+  extension?: IConfig<Conditions, Theme, Utils, Prefix, ThemeMap>
+): TStyledSheet<
+  Conditions & BaseConfig['conditions'],
+  Theme & BaseConfig['theme'],
+  Utils & BaseConfig['utils'],
+  Prefix,
+  ThemeMap & BaseConfig['themeMap']
+> {
+  const mergedConfig = deepMerge(stitches.config, extension);
+  return createCss(mergedConfig) as any;
+}
