969fbbfb07a3bd21fefe013de1000356d6598e73
Craig Cavalier <craigcav@gmail.com>
2021-03-19 10:34:36 -0400

    fix: select list item ignoring mouse clicks in IE
    the `focusin` event was bubbling up beyond the bounds of the portal and being triggered on elements that should not receive focus (in IE11), causing the select list to be dismissed prematurely.

--- src/utils/hooks/useOnFocusOutside.ts
+++ src/utils/hooks/useOnFocusOutside.ts
@@ -2,18 +2,19 @@ import {useCallback} from 'react';
 import useEventListenerOutside from './useEventListenerOutside';
 
 const useOnFocusOutside = (handler, refs: React.RefObject<any>[]) => {
-  const {current: target} = refs[0];
-
   const callback = useCallback(
     (event: FocusEvent) => {
       // Chrome seems to ignore the focusin event bubbling up to elements with a negative tab index
       // however, this is NOT the case in Safari and IE11. For the purposes of this hook, we aren't
       // interested in these events (since the element doesn't receive focus), so we'll ignore them.
-      if (event.eventPhase === Event.BUBBLING_PHASE && (event.target as Node).contains(target))
+      if (
+        event.eventPhase === Event.BUBBLING_PHASE &&
+        refs.some(ref => ref.current && (event.target as Node).contains(ref.current))
+      )
         return;
       handler(event);
     },
-    [handler, target]
+    [handler, refs]
   );
 
   // using 'focusin' instead of 'focus' since 'focus' does not bubble up to the document element
