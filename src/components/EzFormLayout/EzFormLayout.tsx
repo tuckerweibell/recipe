92ca4e1ef275dd89dad28ad7183c891c7de0884b
Craig Cavalier <craigcav@gmail.com>
2021-03-25 17:02:35 -0400

    convert EzFormLayout to stitches

--- src/components/EzFormLayout/EzFormLayout.tsx
+++ src/components/EzFormLayout/EzFormLayout.tsx
@@ -1,10 +1,29 @@
-import styled from '@emotion/styled';
-import './vars.css';
+import React, {forwardRef} from 'react';
+import Style from '@ezcater/snitches';
+import theme from './EzFormLayout.theme.config';
+import {domProps} from '../../utils';
 
-const EzFormLayout = styled.div`
-  && > * + * {
-    margin-top: var(--recipe-form-layout-field-margin);
-  }
-`;
+const layout = theme.css({
+  '&& > * + *': {
+    marginTop: '$form-gap',
+  },
+});
 
+type Props = React.ComponentPropsWithRef<'div'>;
+
+/**
+ * Form layouts are used to arrange fields within a form using standard spacing.
+ */
+const EzFormLayout = forwardRef<HTMLDivElement, Props>((initProps, ref) => {
+  const props = domProps(initProps, layout());
+  return (
+    <Style ruleset={theme}>
+      <div {...props} ref={ref} />
+    </Style>
+  );
+});
+
+/**
+ * @component
+ */
 export default EzFormLayout;
--- src/components/EzFormLayout/__tests__/EzFormLayout.test.tsx
+++ src/components/EzFormLayout/__tests__/EzFormLayout.test.tsx
@@ -23,4 +23,50 @@ describe('EzFormLayout', () => {
     const actual = await axe(container.outerHTML);
     expect(actual).toHaveNoViolations();
   });
+
+  it('should forward className and refs', () => {
+    const ref = React.createRef<HTMLDivElement>();
+    render(
+      <EzFormLayout ref={ref} className="custom-class-name">
+        <EzField label="First field" />
+        <EzField label="Second field" />
+      </EzFormLayout>
+    );
+
+    expect(ref.current.className).toMatch(/custom-class-name/);
+  });
+
+  it('should pass type checking', () => {
+    [
+      {
+        noProps: (
+          <EzFormLayout>
+            <EzField label="First field" />
+            <EzField label="Second field" />
+          </EzFormLayout>
+        ),
+        domProps: (
+          <EzFormLayout
+            // can forward class name
+            className="custom-class"
+            // can pass arbitrary data-* attributes
+            data-custom-tag="foo-bar"
+            // can pass boolean data-* attributes
+            data-bool
+            // can pass aria-* attributes
+            aria-describedby="#description"
+            // can pass html attributes
+            hidden
+            // can forward ref
+            ref={React.createRef()}
+          >
+            <EzField label="First field" />
+            <EzField label="Second field" />
+          </EzFormLayout>
+        ),
+      },
+    ].forEach(() => {});
+
+    expect.assertions(0);
+  });
 });
