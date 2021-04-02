a2c0729e4982e49f460ac6846b41bb28f9980599
danidewitt <danidewitt@gmail.com>
2021-04-02 17:07:19 -0400

    [DEVEX-1082] Convert EzSearchInput from emotion to stitches

--- src/components/EzSearchInput/EzSearchInput.tsx
+++ src/components/EzSearchInput/EzSearchInput.tsx
@@ -1,3 +1,21 @@
-import EzSearchInput from './EzSearchInput.styles';
+import React, {forwardRef} from 'react';
+import Style from '@ezcater/snitches';
+import theme from './EzSearchInput.theme.config';
+import EzTextInput from '../EzField/EzTextInput';
+
+const searchInput = theme.css({
+  paddingLeft: '$search-input-padding-left',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '12px center, calc(100% - 10px) center',
+  backgroundImage: '$search-input-background-image',
+});
+
+type Props = React.InputHTMLAttributes<HTMLInputElement>;
+
+const EzSearchInput = forwardRef<HTMLInputElement, Props>((props, ref) => (
+  <Style ruleset={theme}>
+    <EzTextInput ref={ref} {...props} className={searchInput()} />
+  </Style>
+));
 
 export default EzSearchInput;
--- src/components/EzSearchInput/__tests__/EzSearchInput.test.tsx
+++ src/components/EzSearchInput/__tests__/EzSearchInput.test.tsx
@@ -17,4 +17,27 @@ describe('EzSearchInput', () => {
     const actual = await axe(container.outerHTML);
     expect(actual).toHaveNoViolations();
   });
+
+  it('should pass type checking', () => {
+    [
+      {
+        domProps: (
+          <EzSearchInput
+            // can pass arbitrary data-* attributes
+            data-custom-tag="foo-bar"
+            // can pass boolean data-* attributes
+            data-bool
+            // can pass aria-* attributes
+            aria-describedby="#description"
+            // can pass html attributes
+            hidden
+            // can forward ref
+            ref={React.createRef()}
+          />
+        ),
+      },
+    ].forEach(() => {});
+
+    expect.assertions(0);
+  });
 });
