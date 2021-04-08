8bac5cd8fcfb18377363b7d072690f89ec660ce8
Craig Cavalier <craigcav@gmail.com>
2021-04-07 21:42:03 -0400

    fix: don't set margin on EzTimelineIcon

--- src/components/EzTimeline/EzTimelineIcon.tsx
+++ src/components/EzTimeline/EzTimelineIcon.tsx
@@ -14,29 +14,29 @@ const EzTimelineIcon: React.FC<{icon?: React.ReactElement; className?: string}> 
   icon = <PlaceholderIcon />,
   className,
 }) => (
-  <span
-    className={className}
-    css={{
-      position: 'relative',
-      background: 'var(--recipe-global-color-static-white)',
-      border: `1px solid var(--recipe-alias-border-color)`,
-      height: iconDiameter,
-      width: iconDiameter,
-      borderRadius: '50%',
-      flexShrink: 0,
-      alignItems: 'center',
-      display: 'flex',
-      justifyItems: 'center',
-      alignSelf: 'flex-start',
-      marginTop: 18,
-    }}
-  >
-    {React.cloneElement(icon, {
-      'aria-hidden': true,
-      focusable: false,
-      width: 30,
-      height: 16,
-    })}
+  <span css={{alignSelf: 'flex-start', paddingTop: 18}}>
+    <span
+      className={className}
+      css={{
+        position: 'relative',
+        background: 'var(--recipe-global-color-static-white)',
+        border: `1px solid var(--recipe-alias-border-color)`,
+        height: iconDiameter,
+        width: iconDiameter,
+        borderRadius: '50%',
+        flexShrink: 0,
+        alignItems: 'center',
+        justifyItems: 'center',
+        display: 'flex',
+      }}
+    >
+      {React.cloneElement(icon, {
+        'aria-hidden': true,
+        focusable: false,
+        width: 30,
+        height: 16,
+      })}
+    </span>
   </span>
 );
 
