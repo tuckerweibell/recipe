600950ba0380cb437732787613a7f4f5eaa13453
Craig Cavalier <craigcav@gmail.com>
2021-04-15 20:43:55 -0400

    convert listbox to stitches

--- src/components/EzField/EzListBox.tsx
+++ src/components/EzField/EzListBox.tsx
@@ -1,20 +1,86 @@
-/** @jsx jsx */
 import React, {useRef} from 'react';
-import {jsx} from '@emotion/core';
 import Highlighter from 'react-highlight-words';
+import Style from '@ezcater/snitches';
+import theme from './EzField.theme.config';
 import {useUniqueId, useScrollIntoView} from '../../utils/hooks';
-import {listbox as styles} from './EzSelect.styles';
+import {clsx} from '../../utils';
+
+const popup = theme.css({
+  minWidth: '230px',
+  background: 'white',
+  border: '1px solid #ccc',
+  borderRadius: '6px',
+  width: '$full',
+  overflow: 'scroll',
+  maxHeight: '20rem',
+  boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.1)',
+});
+
+const list = theme.css({
+  listStyle: 'none',
+  margin: '0',
+  padding: '0',
+});
+
+const listItem = theme.css({
+  margin: 0,
+  padding: '$100 $150',
+});
+
+const groupLabel = theme.css({
+  fontWeight: '$bold',
+  fontSize: '$75',
+  lineHeight: '1.3em',
+  color: '$deemphasisText',
+});
+
+const listItemOption = theme.css({
+  color: '$blue600',
+  fontWeight: 'bold',
+  position: 'relative',
+  paddingRight: '2em',
+
+  variants: {
+    selected: {
+      true: {
+        backgroundColor: '$blue600',
+        color: 'white',
+      },
+    },
+    current: {
+      true: {
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center right 12px',
+        backgroundImage:
+          "url(\"data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%238b99a6' viewBox='0 0 24 24' width='12' height='12'%3e%3cpath d='M20.29 2L9 13.57 3.71 8.56 0 12.27 9 21 24 5.71z'/%3e%3c/svg%3e\")",
+      },
+    },
+  },
+  compoundVariants: [
+    {
+      selected: 'true',
+      current: 'true',
+      css: {
+        backgroundImage:
+          "url(\"data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' fill='white' viewBox='0 0 24 24' width='12' height='12'%3e%3cpath d='M20.29 2L9 13.57 3.71 8.56 0 12.27 9 21 24 5.71z'/%3e%3c/svg%3e\")",
+      },
+    },
+  ],
+});
 
 const Option = ({focusedKey, activeOptionRef, setFocusedKey, option, selectedKey, ...props}) => {
   /* eslint-disable jsx-a11y/click-events-have-key-events */
   /* eslint-disable jsx-a11y/mouse-events-have-key-events */
   /* Note: lint doesn't detect the keyboard handler is on the input element, not the list */
+  const current = selectedKey === option.key;
+  const selected = focusedKey === option.key;
   return (
     <li
       role="option"
-      aria-current={selectedKey === option.key}
-      aria-selected={focusedKey === option.key}
-      ref={focusedKey === option.key ? activeOptionRef : undefined}
+      className={clsx(listItem(), listItemOption({current, selected}))}
+      aria-current={current}
+      aria-selected={selected}
+      ref={selected ? activeOptionRef : undefined}
       onMouseOver={() => setFocusedKey(option.key)}
       onClick={() => props.replaceSelection(option.key)}
       id={props.id}
@@ -36,8 +102,8 @@ const OptGroup = props => {
   const id = useUniqueId();
   return (
     <li>
-      <ul role="group" aria-describedby={id}>
-        <li id={id} role="presentation">
+      <ul role="group" aria-describedby={id} className={list()}>
+        <li id={id} role="presentation" className={clsx(groupLabel(), listItem())}>
           {props.group}
         </li>
         {props.children}
@@ -76,9 +142,11 @@ const EzListBox = (props, ref) => {
   );
 
   return (
-    <ul css={styles()} role="listbox" ref={ref} {...domProps}>
-      {hasGroupedOptions(items) ? items.map(renderGroup) : items.map(renderItem)}
-    </ul>
+    <Style ruleset={theme}>
+      <ul className={clsx(popup(), list())} role="listbox" ref={ref} {...domProps}>
+        {hasGroupedOptions(items) ? items.map(renderGroup) : items.map(renderItem)}
+      </ul>
+    </Style>
   );
 };
 
