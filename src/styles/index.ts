a0137551178070f776f2c13b7bcf47c48e901af9
Craig Cavalier <craigcav@gmail.com>
2021-04-29 16:58:28 -0400

    remove unused code paths

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,2 +1,1 @@
-export {default as hideVisually} from './hideVisually';
 export {default as Global, css} from './Global';
--- src/themes/styled.tsx
+++
@@ -1,7 +0,0 @@
-import {Interpolation} from '@emotion/core';
-
-const bps = {medium: '768px', large: '1061px'};
-
-export function mq(bp: string, i: Interpolation): Interpolation {
-  return {[`@media (min-width: ${bp in bps ? bps[bp] : bp})`]: i};
-}
