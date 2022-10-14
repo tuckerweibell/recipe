51d61a46c5615db60cb94b0a255cd56b04f8b4c7
danidewitt <danidewitt@gmail.com>
2022-10-14 14:30:12 -0400

    update comment and color, because i typed it wrong

--- packages/recipe/src/components/theme.config.ts
+++ packages/recipe/src/components/theme.config.ts
@@ -261,7 +261,7 @@ const stitches = createStitches({
 
       // EzHeading
       'heading-text': '$text',
-      'subheading-text': '#56585c', // this is neutral 150 in our new ezTheme, until we convert headings to mui
+      'subheading-text': '#565a5c', // this is neutral 160 in our new ezTheme, until we convert headings to mui
       'heading-blue': '$blue950',
       'heading-green': '$green100',
 
