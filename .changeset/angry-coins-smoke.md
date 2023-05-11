bb2665c31966bdee6e5b0bd31cd92121cebe6c90
Noranda Brown <noranda.brown@ezcater.com>
2023-05-11 16:53:36 -0400

    add playroom snippet for EzChip

---
+++ .changeset/angry-coins-smoke.md
@@ -0,0 +1,5 @@
+---
+'@ezcater/recipe': patch
+---
+
+docs: adds playroom snippet for `EzChip`
---
+++ packages/recipe/src/components/EzChip/Documentation/EzChip.snippets.tsx
@@ -0,0 +1,16 @@
+export const snippets = [
+  {
+    group: 'Data Display',
+    name: 'EzChip',
+    code: `
+      <EzChip
+        label="Chip"
+        color="common.grey130"
+        onClick={undefined}
+        onDelete={undefined}
+        size="medium"
+        variant="filled"
+      />
+    `,
+  },
+];
