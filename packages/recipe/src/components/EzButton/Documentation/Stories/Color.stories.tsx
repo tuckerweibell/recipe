f69d8070c8f1a69461575c77720e07d9eca0140a
Noranda Brown <noranda.brown@ezcater.com>
2023-07-24 10:57:45 -0400

    combine color stories in EzButton

--- packages/recipe/src/components/EzButton/Documentation/Stories/Color.stories.tsx
+++ packages/recipe/src/components/EzButton/Documentation/Stories/Color.stories.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import {type Meta, type StoryObj} from '@storybook/react';
-import {capitalize} from '@mui/material';
-import dedent from 'ts-dedent';
+import {capitalize, Stack} from '@mui/material';
 import EzButton from '../../EzButton';
 import DefaultMeta, {Default} from './Default.stories';
 import {EzButtonMuiProps, EzButtonProps} from '../../EzButton.types';
@@ -15,352 +15,77 @@ const meta: Meta<typeof EzButton> = {
 export default meta;
 type Story = StoryObj<typeof EzButton>;
 
-const storyCode = (color: EzThemeColors, variant: EzButtonMuiProps['variant']) =>
-  dedent`
-    <EzButton
-      color="${color}"
-      variant="${variant}"
-    >
-      ${capitalize(color)}
-    </EzButton>
-  `;
+const colors: EzThemeColors[] = [
+  'primary',
+  'secondary',
+  'neutral',
+  'error',
+  'warning',
+  'info',
+  'success',
+  'common.red100',
+];
 
-export const FilledPrimaryColor: Story = {
+const ColorsRender = (variant: EzButtonMuiProps['variant']) => (
+  <Stack alignItems="center" direction="row" gap={2}>
+    <div>{capitalize(variant)}:</div>
+    {colors.map(color => (
+      <EzButton key={color} color={color} variant={variant}>
+        {capitalize(color)}
+      </EzButton>
+    ))}
+  </Stack>
+);
+
+const ColorsJSX = (variant: EzButtonMuiProps['variant']) =>
+  colors
+    .map(color => `<EzButton color="${color}" variant="${variant}">${capitalize(color)}</EzButton>`)
+    .join('\n');
+
+export const FilledColors: Story = {
   args: {
     ...Default.args,
-    children: 'Primary',
-    color: 'primary',
-  },
+    variant: 'filled',
+  } as EzButtonProps,
   parameters: {
-    docs: {
-      source: {
-        code: dedent`
-        <EzButton color="primary">
-          Primary
-        </EzButton>
-      `,
-      },
-    },
-    playroom: {code: storyCode('primary', 'filled')},
+    docs: {source: {code: ColorsJSX('filled')}},
+    playroom: {code: ColorsJSX('filled')},
   },
+  render: () => ColorsRender('filled'),
 };
 
-export const FilledSecondaryColor: Story = {
+export const OutlinedColors: Story = {
   args: {
     ...Default.args,
-    children: 'Secondary',
-    color: 'secondary',
-  },
-  parameters: {
-    playroom: {code: storyCode('secondary', 'filled')},
-  },
-};
-
-export const FilledNeutralColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Neutral',
-    color: 'neutral',
-  },
-  parameters: {
-    playroom: {code: storyCode('neutral', 'filled')},
-  },
-};
-
-export const FilledErrorColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Error',
-    color: 'error',
-  },
-  parameters: {
-    playroom: {code: storyCode('error', 'filled')},
-  },
-};
-
-export const FilledWarningColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Warning',
-    color: 'warning',
-  },
-  parameters: {
-    playroom: {code: storyCode('warning', 'filled')},
-  },
-};
-
-export const FilledInfoColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Info',
-    color: 'info',
-  },
-  parameters: {
-    playroom: {code: storyCode('info', 'filled')},
-  },
-};
-
-export const FilledSuccessColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Success',
-    color: 'success',
-  },
-  parameters: {
-    playroom: {code: storyCode('success', 'filled')},
-  },
-};
-
-export const FilledCommonColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'common.red100',
-    color: 'common.red100',
-  },
-  parameters: {
-    playroom: {
-      code: dedent`
-        <EzButton
-          color="common.red100"
-          variant="filled"
-        >
-        common.red100
-        </EzButton>
-      `,
-    },
-  },
-};
-
-export const OutlinedPrimaryColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Primary',
-    color: 'primary',
     variant: 'outlined',
   } as EzButtonProps,
   parameters: {
-    docs: {
-      source: {
-        code: dedent`
-        <EzButton
-          color="primary"
-          variant="outlined"
-        >
-          Primary
-        </EzButton>
-      `,
-      },
-    },
-    playroom: {code: storyCode('primary', 'outlined')},
+    docs: {source: {code: ColorsJSX('outlined')}},
+    playroom: {code: ColorsJSX('outlined')},
   },
+  render: () => ColorsRender('outlined'),
 };
 
-export const OutlinedSecondaryColor: Story = {
+export const TextColors: Story = {
   args: {
     ...Default.args,
-    children: 'Secondary',
-    color: 'secondary',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('secondary', 'outlined')},
-  },
-};
-
-export const OutlinedNeutralColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Neutral',
-    color: 'neutral',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('neutral', 'outlined')},
-  },
-};
-
-export const OutlinedErrorColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Error',
-    color: 'error',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('error', 'outlined')},
-  },
-};
-
-export const OutlinedWarningColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Warning',
-    color: 'warning',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('warning', 'outlined')},
-  },
-};
-
-export const OutlinedInfoColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Info',
-    color: 'info',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('info', 'outlined')},
-  },
-};
-
-export const OutlinedSuccessColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Success',
-    color: 'success',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('success', 'outlined')},
-  },
-};
-
-export const OutlinedCommonColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'common.red100',
-    color: 'common.red100',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {
-      code: dedent`
-        <EzButton
-          color="common.red100"
-          variant="outlined"
-        >
-        common.red100
-        </EzButton>
-      `,
-    },
-  },
-};
-
-export const TextPrimaryColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Primary',
-    color: 'primary',
     variant: 'text',
   } as EzButtonProps,
   parameters: {
-    docs: {
-      source: {
-        code: dedent`
-        <EzButton
-          color="primary"
-          variant="text"
-        >
-          Primary
-        </EzButton>
-      `,
-      },
-    },
-    playroom: {code: storyCode('primary', 'text')},
+    docs: {source: {code: ColorsJSX('text')}},
+    playroom: {code: ColorsJSX('text')},
   },
+  render: () => ColorsRender('text'),
 };
 
-export const TextSecondaryColor: Story = {
+export const InlineColors: Story = {
   args: {
     ...Default.args,
-    children: 'Secondary',
-    color: 'secondary',
-    variant: 'text',
+    variant: 'inline',
   } as EzButtonProps,
   parameters: {
-    playroom: {code: storyCode('secondary', 'text')},
+    docs: {source: {code: ColorsJSX('inline')}},
+    playroom: {code: ColorsJSX('inline')},
   },
-};
-
-export const TextNeutralColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Neutral',
-    color: 'neutral',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('neutral', 'text')},
-  },
-};
-
-export const TextErrorColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Error',
-    color: 'error',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('error', 'text')},
-  },
-};
-
-export const TextWarningColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Warning',
-    color: 'warning',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('warning', 'text')},
-  },
-};
-
-export const TextInfoColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Info',
-    color: 'info',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('info', 'text')},
-  },
-};
-
-export const TextSuccessColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'Success',
-    color: 'success',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('success', 'text')},
-  },
-};
-
-export const TextCommonColor: Story = {
-  args: {
-    ...Default.args,
-    children: 'common.red100',
-    color: 'common.red100',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {
-      code: dedent`
-        <EzButton
-          color="common.red100"
-          variant="text"
-        >
-        common.red100
-        </EzButton>
-      `,
-    },
-  },
+  render: () => ColorsRender('inline'),
 };
