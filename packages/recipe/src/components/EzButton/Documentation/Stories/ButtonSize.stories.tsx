d3e4f20ab4c1807dbf785a52112738e9656a5113
Noranda Brown <noranda.brown@ezcater.com>
2023-07-24 09:51:13 -0400

    combine button size stories in EzButton

--- packages/recipe/src/components/EzButton/Documentation/Stories/ButtonSize.stories.tsx
+++ packages/recipe/src/components/EzButton/Documentation/Stories/ButtonSize.stories.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import {type Meta, type StoryObj} from '@storybook/react';
-import {capitalize} from '@mui/material';
+import {Stack, capitalize} from '@mui/material';
 import dedent from 'ts-dedent';
 import EzButton from '../../EzButton';
 import DefaultMeta, {Default} from './Default.stories';
@@ -14,151 +15,58 @@ const meta: Meta<typeof EzButton> = {
 export default meta;
 type Story = StoryObj<typeof EzButton>;
 
-const playroomCode = (
-  size: EzButtonMuiProps['size'],
-  variant: EzButtonMuiProps['variant']
-) => dedent`
-  <EzButton size="${size}" variant="${variant}">
-    ${capitalize(size)}
-  </EzButton>
-`;
+const sizes: EzButtonMuiProps['size'][] = ['small', 'medium', 'large'];
 
-export const FilledButtonSizeSmall: Story = {
+const ButtonSizesRender = (variant: EzButtonMuiProps['variant']) => (
+  <Stack alignItems="center" direction="row" gap={2}>
+    <div>{capitalize(variant)}:</div>
+    {sizes.map(size => (
+      <EzButton key={size} size={size} variant={variant}>
+        {capitalize(size)}
+      </EzButton>
+    ))}
+  </Stack>
+);
+
+const ButtonSizesJSX = (variant: EzButtonMuiProps['variant']) =>
+  sizes
+    .map(size => `<EzButton size="${size}" variant="${variant}">${capitalize(size)}</EzButton>`)
+    .join('\n');
+
+export const FilledButtonSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Small',
-    size: 'small',
-  } as EzButtonProps,
+    variant: 'filled',
+  } as EzButtonMuiProps,
   parameters: {
-    playroom: {code: playroomCode('small', 'filled')},
+    docs: {source: {code: ButtonSizesJSX('filled')}},
+    playroom: {code: ButtonSizesJSX('filled')},
   },
+  render: () => ButtonSizesRender('filled'),
 };
 
-export const FilledButtonSizeMedium: Story = {
+export const OutlinedButtonSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Medium',
-    size: 'medium',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton size="medium">
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: playroomCode('medium', 'filled')},
-  },
-};
-
-export const FilledButtonSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    size: 'large',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: playroomCode('large', 'filled')},
-  },
-};
-
-export const OutlinedButtonSizeSmall: Story = {
-  args: {
-    ...Default.args,
-    children: 'Small',
-    size: 'small',
     variant: 'outlined',
-  } as EzButtonProps,
+  } as EzButtonMuiProps,
   parameters: {
-    playroom: {code: playroomCode('small', 'outlined')},
+    docs: {source: {code: ButtonSizesJSX('outlined')}},
+    playroom: {code: ButtonSizesJSX('outlined')},
   },
+  render: () => ButtonSizesRender('outlined'),
 };
 
-export const OutlinedButtonSizeMedium: Story = {
+export const TextButtonSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Medium',
-    size: 'medium',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton
-            size="medium"
-            variant="outlined"
-          >
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: playroomCode('medium', 'outlined')},
-  },
-};
-
-export const OutlinedButtonSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    size: 'large',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: playroomCode('large', 'outlined')},
-  },
-};
-
-export const TextButtonSizeSmall: Story = {
-  args: {
-    ...Default.args,
-    children: 'Small',
-    size: 'small',
     variant: 'text',
-  } as EzButtonProps,
+  } as EzButtonMuiProps,
   parameters: {
-    playroom: {code: playroomCode('small', 'text')},
+    docs: {source: {code: ButtonSizesJSX('text')}},
+    playroom: {code: ButtonSizesJSX('text')},
   },
-};
-
-export const TextButtonSizeMedium: Story = {
-  args: {
-    ...Default.args,
-    children: 'Medium',
-    size: 'medium',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton
-            size="medium"
-            variant="text"
-          >
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: playroomCode('medium', 'text')},
-  },
-};
-
-export const TextButtonSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    size: 'large',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: playroomCode('large', 'text')},
-  },
+  render: () => ButtonSizesRender('text'),
 };
 
 export const FullWidth: Story = {
