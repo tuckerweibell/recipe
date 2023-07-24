91569a70d991f8bfb834a85da53b507538a130ca
Noranda Brown <noranda.brown@ezcater.com>
2023-07-24 10:21:39 -0400

    combine font size stories in EzButton

--- packages/recipe/src/components/EzButton/Documentation/Stories/FontSize.stories.tsx
+++ packages/recipe/src/components/EzButton/Documentation/Stories/FontSize.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {type Meta, type StoryObj} from '@storybook/react';
-import {capitalize} from '@mui/material';
+import {Stack, capitalize} from '@mui/material';
 import dedent from 'ts-dedent';
 import EzButton from '../../EzButton';
 import DefaultMeta, {Default} from './Default.stories';
-import {EzButtonMuiProps, EzButtonProps} from '../../EzButton.types';
+import {EzButtonMuiProps} from '../../EzButton.types';
 
 const meta: Meta<typeof EzButton> = {
   argTypes: DefaultMeta.argTypes,
@@ -15,256 +15,84 @@ const meta: Meta<typeof EzButton> = {
 export default meta;
 type Story = StoryObj<typeof EzButton>;
 
-const storyCode = (fontSize: EzButtonMuiProps['fontSize'], variant: EzButtonMuiProps['variant']) =>
-  `<EzButton fontSize="${fontSize}" variant="${variant}">${capitalize(fontSize)}</EzButton>`;
+const fontSizes: EzButtonMuiProps['fontSize'][] = ['small', 'medium', 'large'];
 
-export const FilledFontSizeSmall: Story = {
-  args: {
-    ...Default.args,
-    children: 'Small',
-    fontSize: 'small',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('small', 'filled')},
-  },
-};
-
-export const FilledFontSizeMedium: Story = {
-  args: {
-    ...Default.args,
-    children: 'Medium',
-    fontSize: 'medium',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton fontSize="medium">
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: storyCode('medium', 'filled')},
-  },
-};
-
-export const FilledFontSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    fontSize: 'large',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('large', 'filled')},
-  },
-};
-
-export const FilledFontSizeInherit: Story = {
-  args: {
-    ...Default.args,
-    children: 'Inherit',
-    fontSize: 'inherit',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <div style={{fontSize: '20px'}}>
-            <EzButton fontSize="inherit">
-              Inherit
-            </EzButton>
-          </div>
-        `,
-      },
-    },
-    playroom: {
-      code: dedent`
-        <div style={{fontSize: '20px'}}>
-          <EzButton fontSize="inherit">
-            Inherit
-          </EzButton>
-        </div>
-      `,
-    },
-  },
-  render: (args: EzButtonMuiProps) => (
+const FontSizesRender = (variant: EzButtonMuiProps['variant']) => (
+  <Stack alignItems="center" direction="row" gap={2}>
+    <div>{capitalize(variant)}:</div>
+    {fontSizes.map(fontSize => (
+      <EzButton key={fontSize} fontSize={fontSize} variant={variant}>
+        {capitalize(fontSize)}
+      </EzButton>
+    ))}
     <div style={{fontSize: '20px'}}>
-      <EzButton {...args} fontSize="inherit">
+      <EzButton fontSize="inherit" variant={variant}>
         Inherit
       </EzButton>
     </div>
-  ),
+  </Stack>
+);
+
+const FontSizesJSX = (variant: EzButtonMuiProps['variant']) => {
+  return fontSizes
+    .map(
+      fontSize =>
+        `<EzButton fontSize="${fontSize}" variant="${variant}">${capitalize(fontSize)}</EzButton>`
+    )
+    .join('\n').concat(dedent`
+      \n
+      <div style={{fontSize: '20px'}}>
+        <EzButton fontSize="inherit" variant="${variant}">
+          Inherit
+        </EzButton>
+      </div>
+    `);
 };
 
-export const OutlinedFontSizeSmall: Story = {
+export const FilledFontSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Small',
-    fontSize: 'small',
+    variant: 'filled',
+  } as EzButtonMuiProps,
+  parameters: {
+    docs: {source: {code: FontSizesJSX('filled')}},
+    playroom: {code: FontSizesJSX('filled')},
+  },
+  render: () => FontSizesRender('filled'),
+};
+
+export const OutlinedFontSizes: Story = {
+  args: {
+    ...Default.args,
     variant: 'outlined',
-  } as EzButtonProps,
+  } as EzButtonMuiProps,
   parameters: {
-    playroom: {code: storyCode('small', 'outlined')},
+    docs: {source: {code: FontSizesJSX('outlined')}},
+    playroom: {code: FontSizesJSX('outlined')},
   },
+  render: () => FontSizesRender('outlined'),
 };
 
-export const OutlinedFontSizeMedium: Story = {
+export const TextFontSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Medium',
-    fontSize: 'medium',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton
-            fontSize="medium"
-            variant="outlined"
-          >
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: storyCode('medium', 'outlined')},
-  },
-};
-
-export const OutlinedFontSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    fontSize: 'large',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('large', 'outlined')},
-  },
-};
-
-export const OutlinedFontSizeInherit: Story = {
-  args: {
-    ...Default.args,
-    children: 'Inherit',
-    fontSize: 'inherit',
-    variant: 'outlined',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <div style={{fontSize: '20px'}}>
-            <EzButton fontSize="inherit" variant="outlined">
-              Inherit
-            </EzButton>
-          </div>
-        `,
-      },
-    },
-    playroom: {
-      code: dedent`
-        <div style={{fontSize: '20px'}}>
-          <EzButton fontSize="inherit" variant="outlined">
-            Inherit
-          </EzButton>
-        </div>
-      `,
-    },
-  },
-  render: (args: EzButtonMuiProps) => (
-    <div style={{fontSize: '20px'}}>
-      <EzButton {...args} fontSize="inherit">
-        Inherit
-      </EzButton>
-    </div>
-  ),
-};
-
-export const TextFontSizeSmall: Story = {
-  args: {
-    ...Default.args,
-    children: 'Small',
-    fontSize: 'small',
     variant: 'text',
-  } as EzButtonProps,
+  } as EzButtonMuiProps,
   parameters: {
-    playroom: {code: storyCode('small', 'text')},
+    docs: {source: {code: FontSizesJSX('text')}},
+    playroom: {code: FontSizesJSX('text')},
   },
+  render: () => FontSizesRender('text'),
 };
 
-export const TextFontSizeMedium: Story = {
+export const InlineFontSizes: Story = {
   args: {
     ...Default.args,
-    children: 'Medium',
-    fontSize: 'medium',
-    variant: 'text',
-  } as EzButtonProps,
+    variant: 'inline',
+  } as EzButtonMuiProps,
   parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <EzButton
-            fontSize="medium"
-            variant="text"
-          >
-            Medium
-          </EzButton>
-        `,
-      },
-    },
-    playroom: {code: storyCode('medium', 'text')},
+    docs: {source: {code: FontSizesJSX('inline')}},
+    playroom: {code: FontSizesJSX('inline')},
   },
-};
-
-export const TextFontSizeLarge: Story = {
-  args: {
-    ...Default.args,
-    children: 'Large',
-    fontSize: 'large',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    playroom: {code: storyCode('large', 'text')},
-  },
-};
-
-export const TextFontSizeInherit: Story = {
-  args: {
-    ...Default.args,
-    children: 'Inherit',
-    fontSize: 'inherit',
-    variant: 'text',
-  } as EzButtonProps,
-  parameters: {
-    docs: {
-      source: {
-        code: dedent`
-          <div style={{fontSize: '20px'}}>
-            <EzButton fontSize="inherit" variant="text">
-              Inherit
-            </EzButton>
-          </div>
-        `,
-      },
-    },
-    playroom: {
-      code: dedent`
-        <div style={{fontSize: '20px'}}>
-          <EzButton fontSize="inherit" variant="text">
-            Inherit
-          </EzButton>
-        </div>
-      `,
-    },
-  },
-  render: (args: EzButtonMuiProps) => (
-    <div style={{fontSize: '20px'}}>
-      <EzButton {...args} fontSize="inherit">
-        Inherit
-      </EzButton>
-    </div>
-  ),
+  render: () => FontSizesRender('inline'),
 };
